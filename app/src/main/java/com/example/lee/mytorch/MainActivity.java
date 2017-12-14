3e6e22773da60f185fd21d76006108f2da504475
LeeeFeng <1149116763@qq.com>
2017-12-14 22:42:27 +0800

    version1:开关控制手电的开关

--- app/src/main/java/com/example/lee/mytorch/MainActivity.java
+++ app/src/main/java/com/example/lee/mytorch/MainActivity.java
@@ -1,13 +1,65 @@
 package com.example.lee.mytorch;
 
+import android.content.Context;
+import android.graphics.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CameraManager mCameraManager;
+    private Camera mCamera=null;
+    boolean flag=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnOn=(Button)findViewById(R.id.lightOn);
+        Button btnOff=(Button)findViewById(R.id.lightOff);
+        mCameraManager=(CameraManager)getSystemService(Context.CAMERA_SERVICE);
+        try {
+            String[] cameraList=mCameraManager.getCameraIdList();
+            for(String str:cameraList){
+            }
+        }catch (CameraAccessException e){
+            Log.e("error",e.getMessage());
+        }
+
+        btnOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lightSwitch(false);
+            }
+        });
+
+        btnOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lightSwitch(true);
+            }
+        });
+    }
+
+    private void lightSwitch(final boolean lightStatus) {
+        if(lightStatus){
+                try{
+                    mCameraManager.setTorchMode("0",false);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+        }
+        else{
+            try{
+                mCameraManager.setTorchMode("0",true);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
